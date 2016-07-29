b14dd222b760f8a3bf084a12afee419c1cdf551f
suzeyu <suzeyu_1992@163.com>
2016-07-29 14:41:11 +0800

    first init

---
+++ index.android.js
@@ -0,0 +1,43 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+class IsRN extends Component {
+  render() {
+    return (
+        <Text>Hello Word</Text>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+// 注意，这里用引号括起来的'IsRN'必须和你init创建的项目名一致
+AppRegistry.registerComponent('IsRN', () => IsRN);
